6ee56a41bcbbc6a88b97c160864c4abd7b8dfe50
mvanek <matt.vanek@gmail.com>
2012-11-30 19:58:01 -0600

    swapped reverselist.c bugs

--- reverselist.c
+++ reverselist.c
@@ -4,15 +4,8 @@
 typedef struct list_node
 {
     int element;
-    int is_head;
     struct list_node* next;
 } list_node_t;
-
-void* mark_head(list_node_t* head)
-{
-    head->is_head = 1;
-    return head;
-}
 
 /**
  * This function makes a linked list from an array, and returns the head
@@ -25,14 +18,13 @@ void* mark_head(list_node_t* head)
  */
 void* make_linked_list(int* array, int length)
 {
-    list_node_t head;
-    list_node_t* next_node = &head;
+    list_node_t* head = malloc( sizeof(list_node_t) );
+    list_node_t* next_node = head;
     int i;
 
     for (i=0; i < length; i++)
     {
         next_node->element = array[i];
-        next_node->is_head = 0;
         if (i != length-1)
         {
             next_node->next = (list_node_t*) malloc(sizeof(list_node_t));
@@ -42,7 +34,7 @@ void* make_linked_list(int* array, int length)
             next_node->next = NULL;
     }
 
-    return mark_head(&head);
+    return head;
 }
 
 list_node_t* reverse_linked_list(list_node_t* head)
@@ -56,7 +48,7 @@ list_node_t* reverse_linked_list(list_node_t* head)
     next_node = head->next;
     head->next = NULL;
     
-    while (next_node != NULL)
+    while (last_node != NULL)
     {
         new_next_node = next_node->next;
         next_node->next = last_node;
@@ -81,17 +73,17 @@ void print_linked_list(list_node_t* head)
 }
 
 /**
- * A simple routine which frees each noe of a linked list
+ * A simple routine which frees each node of a linked list
  *
  * @param head the head of the linked list
  */
 void free_linked_list(list_node_t* head)
 {
-    list_node_t* next_node = head;
+    list_node_t* next_node = head; list_node_t* temp = next_node;
 
     while (next_node != NULL)
     {
-        list_node_t* temp = next_node;
+        temp = next_node;
         next_node = next_node->next;
         free(temp);
     }
@@ -103,8 +95,11 @@ int main()
     int numints = 10;
     
     list_node_t* head = make_linked_list(myints, numints);
+    printf("original:\n");
+    print_linked_list(head);
     head = reverse_linked_list(head);
 
+    printf("\nreversed:\n");
     print_linked_list(head);
     fflush(stdout);
 
